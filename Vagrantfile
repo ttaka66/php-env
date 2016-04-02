42492fc6c12ebe615d3b59a6def89c8868f9a5ee
ttaka66 <d.m_ro.nl.e@icloud.com>
2016-04-02 15:07:25 +0900

    postgresql added

--- Vagrantfile
+++ Vagrantfile
@@ -86,6 +86,7 @@ Vagrant.configure(2) do |config|
       recipe[yum-epel]
       recipe[nginx]
       recipe[php-env::php55]
+      recipe[postgresql]
     ]
   end
 
---
+++ site-cookbooks/postgresql/recipes/default.rb
@@ -0,0 +1,17 @@
+package "postgresql" do
+  action :install
+end
+
+package "postgresql-server" do
+  action :install
+end
+
+execute "postgrosql-init" do
+  not_if "test -f /var/lib/pgsql/data/postgresql.conf"
+  command "service postgresql initdb"
+  action :run
+end
+
+service "postgresql" do
+  action [:enable , :start]
+end
